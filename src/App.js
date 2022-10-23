a65d75d8a9e607906db560100e50fb1cb4293172
Muhammad Husnain <78450824+devhusnain@users.noreply.github.com>
2022-10-23 10:27:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 function App() {
   return (
     <div className="App">
-      <GoogleReCaptchaProvider reCaptchaKey="6LcA_3siAAAAAKBvnc9NRuHr3SDicVhOfTlmr2O1">
+      <GoogleReCaptchaProvider reCaptchaKey="_Private_site_key_">
         <GoogleRecaptchaComponent />
       </GoogleReCaptchaProvider>
     </div>
